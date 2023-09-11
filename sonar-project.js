a298583e8c65273389b72e3c76bc7e12c8f2ef1b
Vamsi_vemula <vamsi.v@pycube.com>
2023-09-11 13:43:21 -0400

    some

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-	serverUrl: 'http://54.164.176.191:9000/projects',
+	serverUrl: 'http://54.164.176.191:9000/',
 	options: {
 		'sonar.projectDescription': 'This is a Node JS application',
 		'sonar.projectName': 'Node-Js-Sonar',
